3cb41dfdbfb17cd861c421cfa363a034fd7a803e
nl3appdev21 <56339045+nl3appdev21@users.noreply.github.com>
2021-08-15 18:02:09 -0400

    checkpoint

---
+++ NJRealtorExam/ExamItem.swift
@@ -0,0 +1,21 @@
+//
+//  ExamItem.swift
+//  NJRealtorExam
+//
+//  Created by user on 8/1/21.
+//
+
+import Foundation
+
+struct ExamItem: Codable {
+    
+    // define fields and data types
+    var id: Int
+    var category: String
+    var question: String
+    // var answers: String // use for njrealtorexam_12a.json with out array
+    var answers: [String] // use for njrealtorexam_12.json with array
+    var correct: Int
+    var details: String
+    
+}
