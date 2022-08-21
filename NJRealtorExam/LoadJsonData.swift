010f3bf53acb83b673772964301e5ce975440412
nl3appdev21 <56339045+nl3appdev21@users.noreply.github.com>
2022-08-20 20:10:10 -0400

    updated json file
    updated json file

--- NJRealtorExam/LoadJsonData.swift
+++ NJRealtorExam/LoadJsonData.swift
@@ -16,7 +16,7 @@ public class LoadJsonData {
     }
     
     func load() {
-        if let fileLocation = Bundle.main.url(forResource: "njrealtorexam_12", withExtension: "json") {
+        if let fileLocation = Bundle.main.url(forResource: "njrealtorexam_21", withExtension: "json") {
             do {
                 let data = try Data(contentsOf: fileLocation)
                 let jsonDecoder = JSONDecoder()
